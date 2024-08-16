221c982f707845b3cd8c6dcf883e7112d75466f9
mad-cost <mad_scientist1998@naver.com>
2024-08-16 16:25:46 +0900

    Queue에 payment Consumer 연결하고 메시지 받아오기

---
+++ payment/src/main/java/com/market/payment/PaymentEndpoint.java
@@ -0,0 +1,22 @@
+package com.market.payment;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PaymentEndpoint {
+
+  @Value("${spring.application.name}")
+  public String appName;
+
+  // RabbitMQ에서 메시지를 수신하기 위해 사용하는 기능 / 지정된 큐로부터 메시지를 받아 처리
+  @RabbitListener(queues = "${message.queue.payment}")
+  public void receiveMessage(String orderId){ // 큐로부터 orderId 값을 받는다
+    log.info("receive OrderId: {}, appName: {}", orderId, appName);
+  }
+
+
+}
